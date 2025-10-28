3124b974090c62694c2062830deed40f6bddb178
Ludvinae <benoit.champlon79@gmail.com>
2025-10-28 11:40:31 +0100

    implemented print data function

--- file.py
+++ file.py
@@ -12,8 +12,11 @@ def file_read():
         reader = csv.DictReader(file)
         rows = list(reader)
 
+        return rows
+
 
 def add_anime(anime: dict):
-    with open("anime_database.csv", "w", encoding="utf-8", newline="") as file:
-        writer = csv.DictWriter(file)
-        writer.writerows(anime)
+    with open("anime_database.csv", "a", encoding="utf-8", newline="") as file:
+        champs = ["titre", "année", "genre", "vu"]
+        writer = csv.DictWriter(file, fieldnames=champs)
+        writer.writerow(anime)
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import input
 import file
 
-data = [{"titre": "Inception","année": 2010,"genre": "SF","vu": True}, 
-        {"titre": "Titanic","année": 1997,"genre": "Romance","vu": False}]
+data = [{"titre": "Inception","année": 2010,"genre": "SF","vu": 1}, 
+        {"titre": "Titanic","année": 1997,"genre": "Romance","vu": 0}]
 
 def main():
     if not os.path.exists("anime_database.csv"):
@@ -13,13 +13,14 @@ def main():
         if input.file_overwrite():
             file.file_creation(data)
 
+    animeList = read_anime_list()
+    os.system("cls")
+    for row in animeList:
+        print(anime_to_string(row))
     while True:
         ajouter_anime()
-    
-    
-
-
-
+        break
+   
 
 
 def ajouter_anime():
@@ -29,6 +30,16 @@ def ajouter_anime():
 
     file.add_anime(anime)
 
+def read_anime_list():
+    data = file.file_read()
+    return data
+
+def anime_to_string(row):
+    vu = ", vu" if int(row['vu']) else ""
+    return f"{row['titre']}: anime de type {row['genre']} sorti en {row['année']}{vu}."
+
+def rechercher_film(film):
+    pass
 
 
 if __name__ == '__main__':
